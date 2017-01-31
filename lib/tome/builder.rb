1b676c7566c480443d7413b136743d4f09432387
Micah Geisel <micah@botandrose.com>
2017-01-31 14:45:33 -0800

    remove global state from Tome::Builder and simplify.

--- lib/tome/builder.rb
+++ lib/tome/builder.rb
@@ -1,68 +1,39 @@
 module Tome
-  class Builder
-    cattr_accessor(:builders) { Hash.new }
-
-    def self.register klass, &block
-      builder = Class.new(TableBuilder) do
-        factory klass
-        instance_eval &block
-      end
-      builders[klass] = builder
-    end
-
+  class Builder < Struct.new(:table, :klass, :block)
     def self.build! table, klass, &block
-      # subclass to prevent multiple blocks from stomping on each other
-      klass = Class.new(klass)
-      if block_given? and !builders[klass]
-        register klass, &block
-      end
-      builders[klass].build!(table)
+      new(table, klass, block).build!
     end
-  end
 
-  class TableBuilder < Struct.new(:table)
+    attr_accessor :transformations
+
+    def initialize(*)
+      super
+      self.transformations = []
+      underscore_keys
+      instance_eval &block if block.respond_to?(:call)
+    end
+
     def build!
       table.hashes.map do |attributes|
         transformations.each { |transformation| transformation.call(attributes) }
-        factory.create! attributes
+        klass.create! attributes
       end
     end
 
-    class_attribute :transformations, :factory_klass
-    self.transformations = []
-
-    def self.transformation &block
+    def transformation &block
       transformations << block
     end
 
-    def self.factory klass=nil
-      if klass.nil?
-        factory_klass || default_klass
-      else
-        self.factory_klass = klass
+    def underscore_keys
+      transformation do |attributes|
+        new_attributes = attributes.inject({}) do |hash, (key, value)|
+          hash.merge key.parameterize.underscore => value
+        end
+        attributes.replace new_attributes
       end
     end
 
-    def self.default_klass
-      to_s.sub("Builder", "").constantize
-    end
-
-    def self.build! table
-      new(table).build!
-    end
-
-    def factory
-      self.class.factory
-    end
-
-    transformation do |attributes|
-      new_attributes = attributes.inject({}) do |hash, (key, value)|
-        hash.merge key.parameterize.underscore => value
-      end
-      attributes.replace new_attributes
-    end
-
-    def self.has_attached_file *keys
+    def has_attached_file *keys
       keys.each do |key|
         key = key.to_s
         transformation do |attributes|
