aa6734e88d575ffaa8876f0496e00443add381ea
Micah Geisel <micah@botandrose.com>
2017-03-22 23:08:11 -0700

    add missing tests for DSL methods.

--- spec/chop/dsl_spec.rb
+++ spec/chop/dsl_spec.rb
@@ -1,3 +1,4 @@
+require 'cucumber'
 require 'chop/dsl'
 
 describe Chop::DSL do
@@ -23,6 +24,42 @@ describe Chop::DSL do
       expect(session).to receive(:find).with(selector).and_return(double(tag_name: "table"))
       expect(table_class).to receive(:diff!).with(selector, table, session: session, &block)
       subject.diff! selector, table, session: session, &block
+    end
+  end
+
+  describe ".fill_in!" do
+    it "delegates to Chop::Form.fill_in!" do
+      table = double
+      klass = stub_const("Chop::Form", double)
+      expect(klass).to receive(:fill_in!).with(table)
+      subject.fill_in! table
+    end
+  end
+
+  describe "monkeypatched methods on Cucumber::MultilineArgument::DataTable" do
+    let(:table) { Cucumber::MultilineArgument::DataTable.from([[]]) }
+
+    describe "#create!" do
+      it "delegates to Chop.create" do
+        klass = double
+        expect(Chop).to receive(:create!).with(klass, table)
+        table.create!(klass)
+      end
+    end
+
+    describe "#diff!" do
+      it "delegates to Chop.diff!" do
+        selector = "table"
+        expect(Chop).to receive(:diff!).with(selector, table)
+        table.diff!(selector)
+      end
+    end
+
+    describe "#fill_in!" do
+      it "delegates to Chop.fill_in!" do
+        expect(Chop).to receive(:fill_in!).with(table)
+        table.fill_in!
+      end
     end
   end
 end
